d3101c77393ac8e70c5275c358b1a707a3a5c93b
Parker Ryan <aparkerryan27@gmail.com>
2021-11-08 12:14:42 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Interactive Cookbook
+The Interactive Cookbook will take AllRecipes URL inputs and accept modification parameters to return a different take on the original recipe
+
+## Group 
+Daniel Oh, Alex Kaiser, Parker Ryan
+
+## Installing dependencies
+```
+$ pip3 install -r requirements.txt   // Install required packages
+```
+
+## Running the program
+.......
+
+## Other Code Sources
+For the database of recipe keywords, part of the DB was gathered from this former project (this was mainly to save time with typing): https://github.com/rojaswestall/cs337/blob/master/project2/kb.json
+
+For the vegetarian and non-vegetarian functions, part of the DBs were gathered from this former project for the same reason: https://github.com/amitadate/EECS-337-NLP-Project-02/blob/master/Final_Submission/transformation_list.py?fbclid=IwAR3kkLEU2g6rs-h6_ujLkpmh6vL4znWXgO-zsUU8BTeoz2ypWNQlMchiRFM
--- parsePage.py
+++ parsePage.py
@@ -1,4 +1,7 @@
+#Use BeautifulSoup4 to Parse an AllRecipes page
+
 import requests
+import nltk
 from bs4 import BeautifulSoup
 
 #Allows us to imitate a real request more smoothly so rq is not blocked
@@ -16,13 +19,44 @@ req = requests.get(url, headers)
 soup = BeautifulSoup(req.content, 'html.parser')
 #print(soup.prettify())
 
+
+#ingredients
 ingredient_objects = soup.find_all("span", class_="ingredients-item-name")
 ingredients = [ingredient.getText() for ingredient in ingredient_objects]
 print(ingredients)
 
+#instructions
 instructions_section = soup.find("ul", class_="instructions-section")
 instructions_objects = instructions_section.find_all("p")
 instructions_text = [instruct.getText() for instruct in instructions_objects]
 print(instructions_text)
 
 
+#overview items
+overview_items = soup.find_all("div", class_="recipe-meta-item-header")
+objects = ["" + item.getText() + " " + item.next_sibling.next_sibling.getText() for item in overview_items]
+print(objects)
+title = soup.find("h1", class_="headline").getText()
+print(title)
+
+
+
+
+#Content to Look at for Transformations
+#https://www.allrecipes.com/article/common-ingredient-substitutions/
+
+
+
+#print an ouput of the new recipe
+def print_recipe(): 
+    print("Recipe: " + title)
+    print("Ingredients: ")
+    for i in ingredients:
+        print(i + "\n")
+
+    print("\n")
+    print("Steps: ")
+    for step in len(instructions_text): 
+        print(step + ". " + instructions_text[step] + "\n")
+
+    
